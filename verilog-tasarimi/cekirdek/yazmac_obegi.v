70b8bdca5d3bc3cb6e5c15e093d0fb55b1374ba2
celuk <seyyid4091@gmail.com>
2023-01-09 01:35:41 +0300

    Yazmac obegini ekle

--- verilog-tasarimi/cekirdek/yazmac_obegi.v
+++ verilog-tasarimi/cekirdek/yazmac_obegi.v
@@ -5,10 +5,35 @@
 
 module yazmac_obegi(
     input clk_i,
-    input rst_i
+    input rst_i,
 
+    input [4:0] ky1_adres_i,
+    input [4:0] ky2_adres_i,
+    input [4:0] hy_adres_i,
 
+    input [31:0] hy_deger_i,
+
+    input yaz_i,
+
+    output reg [31:0] ky1_deger_o,
+    output reg [31:0] ky2_deger_o
 );
+    
+    reg [31:0] yazmac_obegi [31:0];
 
+    always@* begin
+        ky1_deger_o = yazmac_obegi[ky1_adres_i];
+        ky2_deger_o = yazmac_obegi[ky2_adres_i];
+    end
 
+    integer i = 0;
+    always@(posedge clk_i) begin
+        if (rst_i) begin
+            for(i = 0; i < 32; i = i + 1)
+                yazmac_obegi[i] <= 0;
+        end
+        else if(yaz_g) begin
+            yazmac_obegi[hy_adres_i] <= hy_deger_i;
+        end
+    end
 endmodule
