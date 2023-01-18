c6ee3d3bd0cee99cb50dd13bd3c4ffb6e5f79d2c
egedevecii <113840264+egedevecii@users.noreply.github.com>
2023-01-18 23:11:31 +0300

    tb_bolme_birimi eklendi

--- verilog-simulasyonu/cekirdek/boru_hatti/yurut/tb_bolme_birimi.v
+++ verilog-simulasyonu/cekirdek/boru_hatti/yurut/tb_bolme_birimi.v
@@ -12,7 +12,7 @@ wire [31:0] bolum_o;
 bolme_birimi bolb (
     .bolunen_i(bolunen_i),
     .bolen_i(bolen_i), 
-	.bolum_o(bolum_o)
+    .bolum_o(bolum_o)
 );
 
 initial begin
@@ -21,19 +21,19 @@ initial begin
 	bolen_i =   32'd5;
     #10;
 		
-	//2048 / 16 = 128
+    //2048 / 16 = 128
     bolunen_i = 32'd2048;
-	bolen_i =   32'd16;
+    bolen_i =   32'd16;
     #10;
 		
-	//67 / 6 = 11
-	bolunen_i = 32'd67;
-	bolen_i =   32'd6;
+    //67 / 6 = 11
+    bolunen_i = 32'd67;
+    bolen_i =   32'd6;
     #10;
         
     //111 / 112 = 0
     bolunen_i = 32'd111;
-	bolen_i =   32'd112;
+    bolen_i =   32'd112;
     #10;
         
     //90 / 33 = 2
