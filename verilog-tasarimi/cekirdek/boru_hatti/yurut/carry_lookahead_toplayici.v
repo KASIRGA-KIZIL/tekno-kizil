fe9193f04f255f272e18e997f562c4ba417fbb88
egedevecii <113840264+egedevecii@users.noreply.github.com>
2023-01-20 12:22:38 +0300

    carry_lookahead_toplayici islem girisi eklendi

--- verilog-tasarimi/cekirdek/boru_hatti/yurut/carry_lookahead_toplayici.v
+++ verilog-tasarimi/cekirdek/boru_hatti/yurut/carry_lookahead_toplayici.v
@@ -4,20 +4,33 @@
 `include "tanimlamalar.vh"
 
 module carry_lookahead_toplayici #(parameter BIT = 32)(
-input  [BIT-1:0] a_i, b_i,
-input   giren_elde_i,
+input  [BIT-1:0] a_i, b_i, 
+input  islem_i,
 output reg [BIT-1:0] toplam_o
 );
 
 reg [BIT-1:0] elde_r=0;
+wire [BIT-1:0] cikan_w= ~b_i;
 integer i;
-
+  
 always@(*)begin
-    elde_r[0] = a_i[0] & b_i[0];
-    toplam_o[0] = a_i[0] ^ b_i[0] ^ giren_elde_i;
-    for ( i = 1; i < BIT; i=i+1) begin
-        elde_r[i] = (a_i[i] & b_i[i]) | (a_i[i] & elde_r[i-1]) | (b_i[i] & elde_r[i-1]);
-        toplam_o[i] = a_i[i] ^ b_i[i] ^ elde_r[i-1];
+    if(islem_i)begin
+        elde_r[0] = (a_i[0] & cikan_w[0]) | (a_i[0] & 1) | (cikan_w[0] & 1);
+        toplam_o[0] = a_i[0] ^ cikan_w[0] ^ 1;
+        for ( i = 1; i < BIT; i=i+1) begin
+            elde_r[i] = (a_i[i] & cikan_w[i]) | (a_i[i] & elde_r[i-1]) | (cikan_w[i] & elde_r[i-1]);
+            toplam_o[i] = a_i[i] ^ cikan_w[i] ^ elde_r[i-1];
+        end
+    
+    end
+    else begin
+        elde_r[0] = a_i[0] & b_i[0];
+        toplam_o[0] = a_i[0] ^ b_i[0];
+        for ( i = 1; i < BIT; i=i+1) begin
+            elde_r[i] = (a_i[i] & b_i[i]) | (a_i[i] & elde_r[i-1]) | (b_i[i] & elde_r[i-1]);
+            toplam_o[i] = a_i[i] ^ b_i[i] ^ elde_r[i-1];
+        end
     end
 end
+
 endmodule
