95f10e67b7cfc1029ab5f83e6c30fdb38c32f337
hakan-demirli <emre.demirli@metu.edu.tr>
2023-01-25 17:06:42 +0300

    yurut tb'yi gelistir

--- verilog-simulasyonu/cekirdek/boru_hatti/yurut/tb_yurut.v
+++ verilog-simulasyonu/cekirdek/boru_hatti/yurut/tb_yurut.v
@@ -2,31 +2,59 @@
 `timescale 1ns / 1ps
 
 `include "tanimlamalar.vh"
+module tb_yurut;
 
-module tb_yurut();
+// Parameters
 
-    reg clk_i = 0;
-    reg rst_i = 0;
+// Ports
+reg  clk_i = 0;
+reg  rst_i = 0;
+reg [`MI_BIT-1:0] cyo_mikroislem_i;
+reg [        4:0] cyo_rd_adres_i;
+reg [       31:0] cyo_ps_artmis_i;
+reg [       31:0] cyo_deger1_i;
+reg [       31:0] cyo_deger2_i;
+reg  cyo_yapay_zeka_en_i = 0;
+reg [ 2:0] cyo_lt_ltu_eq_i;
+reg [ 1:0] cyo_buyruk_tipi_i;
+wire [31:0] gtr_atlanan_ps_o;
+wire  gtr_atlanan_ps_gecerli_o;
+wire [ 4:0] gy_rd_adres_o;
+wire [31:0] gy_ps_artmis_o;
+wire [31:0] gy_rd_deger_o;
+wire [31:0] gy_bib_deger_o;
+wire [ 2:0] gy_mikroislem_o;
+wire [31:0] cyo_yonlendir_deger_o;
 
-    yurut yrt(
-        .clk_i(clk_i),
-        .rst_i(rst_i)
+yurut
+yurut_dut (
+  .clk_i (clk_i ),
+  .rst_i (rst_i ),
+  .cyo_mikroislem_i (cyo_mikroislem_i ),
+  .cyo_rd_adres_i (cyo_rd_adres_i ),
+  .cyo_ps_artmis_i (cyo_ps_artmis_i ),
+  .cyo_deger1_i (cyo_deger1_i ),
+  .cyo_deger2_i (cyo_deger2_i ),
+  .cyo_yapay_zeka_en_i (cyo_yapay_zeka_en_i ),
+  .cyo_lt_ltu_eq_i (cyo_lt_ltu_eq_i ),
+  .cyo_buyruk_tipi_i (cyo_buyruk_tipi_i ),
+  .gtr_atlanan_ps_o (gtr_atlanan_ps_o ),
+  .gtr_atlanan_ps_gecerli_o (gtr_atlanan_ps_gecerli_o ),
+  .gy_rd_adres_o (gy_rd_adres_o ),
+  .gy_ps_artmis_o (gy_ps_artmis_o ),
+  .gy_rd_deger_o (gy_rd_deger_o ),
+  .gy_bib_deger_o (gy_bib_deger_o ),
+  .gy_mikroislem_o (gy_mikroislem_o ),
+  .cyo_yonlendir_deger_o  ( cyo_yonlendir_deger_o)
+);
 
+initial begin
+  begin
+    $finish;
+  end
+end
 
-    );
-
-    always begin
-        #5;
-        clk_i = ~clk_i;
-    end
-
-    initial begin
-        rst_i = 1'b1;
-        #100;
-        rst_i = 0;
-
-
-        $finish;
-    end
+always
+  #5  clk_i = ! clk_i ;
 
 endmodule
--- verilog-tasarimi/cekirdek/boru_hatti/yurut/yurut.v
+++ verilog-tasarimi/cekirdek/boru_hatti/yurut/yurut.v
@@ -9,37 +9,29 @@ module yurut(
     input wire clk_i,
     input wire rst_i,
 
-    // Veri yolu icin
-    input  wire [`MI_BIT-1:0] mikroislem_i,
-    input  wire [ 4:0] rd_adres_i,               // Rd'nin adresi
-    input  wire [31:0] program_sayaci_artmis_i,  // Rd=PC+4/2 islemi icin gerekli
-    input  wire [31:0] deger1_i,                 // Islem birimi girdileri. Yonlendirme ve Immediate secilmis son degerler.
-    input  wire [31:0] deger2_i,
-    input  wire        yapay_zeka_en_i,          // yapay zeka buyruklari rs2 enable biti
+    // Coz-Yazmacoku bolumu sinyallleri
+    input  wire [`MI_BIT-1:0] cyo_mikroislem_i,
+    input  wire [        4:0] cyo_rd_adres_i,               // Rd'nin adresi
+    input  wire [       31:0] cyo_ps_artmis_i,              // Rd=PC+4/2 islemi icin gerekli
+    input  wire [       31:0] cyo_deger1_i,                 // Islem birimi girdileri. Yonlendirme ve Immediate secilmis son degerler.
+    input  wire [       31:0] cyo_deger2_i,
+    input  wire               cyo_yapay_zeka_en_i,          // yapay zeka buyruklari rs2 enable biti
 
     // Branch ve Jump buyruklari icin. Hepsi ayni cevrimde gidecek
-    input  wire [ 2:0] lt_ltu_eq_i,             // Degerler arasindaki iliski. lt_ltu_eq_i: {lessthan,lt_unsigned, equal}
-    input  wire [ 1:0] buyruk_tipi_i,           // J veya B veya digertip. Eger J tipiyse direkt atlanilacak. B tipiyse kosula bakilcak.
-    output wire [31:0] yeni_program_sayaci_o,   // Atlanilan yeni program sayaci, pc+imm veya rs1+imm degerini tasiyor.
-    output wire program_sayaci_gecerli_o,       // Yeni program sayacinin gecerli olup olmadiginin sinyali. J tipinde hep gecerli
-
-    // Dallanma Ongorucu icin. Hepsi ayni cevrimde gidecek
-    input  wire [31:0] program_sayaci_i,        // Suanki buyrugun PC'si. Branch Target Buffer icin gerekli.
-    input  wire [31:0] coz_program_sayaci_artmis_i,    // Tahmin dogru mu bakmak icin. COZ'un PC'si. Cozdeki buyruga ait.
-    output wire [31:0] yurut_program_sayac_o,   // Atlamayi isteyen buyrugun PC'si. Yurutteki buyruga ait.
-    output wire        tahmin_dogru_o,          // COZ'deki PC yuruttekine esit mi? Esitse dogru tahmin
-    output wire [ 1:0] buyruk_tipi_o,           // Jtipi mi B tipi mi.
-    output wire [31:0] program_sayaci_o,        // Suanki buyrugun PC'si. Branch Target Buffer icin gerekli.
+    input  wire [ 2:0] cyo_lt_ltu_eq_i,                // Degerler arasindaki iliski. cyo_lt_ltu_eq_i: {lessthan,lt_unsigned, equal}
+    input  wire [ 1:0] cyo_buyruk_tipi_i,              // J veya B veya digertip. Eger J tipiyse direkt atlanilacak. B tipiyse kosula bakilcak.
+    output wire [31:0] gtr_atlanan_ps_o,               // Atlanilan yeni program sayaci, pc+imm veya rs1+imm degerini tasiyor.
+    output wire        gtr_atlanan_ps_gecerli_o,       // Yeni program sayacinin gecerli olup olmadiginin sinyali. J tipinde hep gecerli
 
     // GERIYAZ icin
-    output reg [ 4:0] rd_adres_o,              // Rd'nin adresi
-    output reg [31:0] program_sayaci_artmis_o, // Rd=PC+4/2 islemi icin gerekli
-    output reg [31:0] rd_deger_o,              // islem birimlerinden cikan sonuc
-    output reg [31:0] bib_deger_o,             // Bellek Islem Biriminin ciktisi.
-    output reg [ 2:0] mikroislem_o,            // Rd secimi ve write enable sinyalleri
+    output reg [ 4:0] gy_rd_adres_o,              // Rd'nin adresi
+    output reg [31:0] gy_ps_artmis_o,             // Rd=PC+4/2 islemi icin gerekli
+    output reg [31:0] gy_rd_deger_o,              // islem birimlerinden cikan sonuc
+    output reg [31:0] gy_bib_deger_o,             // Bellek Islem Biriminin ciktisi.
+    output reg [ 2:0] gy_mikroislem_o,            // Rd secimi ve write enable sinyalleri
 
     // Yonlendirme icin
-    output wire [31:0] yonlendir_deger_o
+    output wire [31:0] cyo_yonlendir_deger_o
 );
 
     // hepsinde sonuc olmayacak duzenlemek lazim
@@ -51,50 +43,47 @@ module yurut(
     wire [31:0] sistem_sonuc_w   ;
 
     aritmetik_mantik_birimi amb (
-        .kontrol(mikroislem_i[`AMB]),
-        .deger1_i(deger1_i),
-        .deger2_i(deger2_i),
-        .lt_ltu_i(lt_ltu_eq_i[2:1]),
+        .kontrol(cyo_mikroislem_i[`AMB]),
+        .deger1_i(cyo_deger1_i),
+        .deger2_i(cyo_deger2_i),
+        .lt_ltu_i(cyo_lt_ltu_eq_i[2:1]),
         .sonuc_o(amb_sonuc_w)
     );
 
-    wire [31:0] rd_deger_sonraki_w = (mikroislem_i[`BIRIM] == `BIRIM_AMB      ) ? amb_sonuc_w      :
-                                     (mikroislem_i[`BIRIM] == `BIRIM_CARPMA   ) ? carp_sonuc_w     :
-                                     (mikroislem_i[`BIRIM] == `BIRIM_BOLME    ) ? bol_sonuc_w      :
-                                     (mikroislem_i[`BIRIM] == `BIRIM_SIFRELEME) ? sifreleme_sonuc_w:
-                                     (mikroislem_i[`BIRIM] == `BIRIM_YAPAYZEKA) ? yapayzeka_sonuc_w:
-                                     (mikroislem_i[`BIRIM] == `BIRIM_SISTEM   ) ? sistem_sonuc_w   :
+    wire [31:0] rd_deger_sonraki_w = (cyo_mikroislem_i[`BIRIM] == `BIRIM_AMB      ) ? amb_sonuc_w      :
+                                     (cyo_mikroislem_i[`BIRIM] == `BIRIM_CARPMA   ) ? carp_sonuc_w     :
+                                     (cyo_mikroislem_i[`BIRIM] == `BIRIM_BOLME    ) ? bol_sonuc_w      :
+                                     (cyo_mikroislem_i[`BIRIM] == `BIRIM_SIFRELEME) ? sifreleme_sonuc_w:
+                                     (cyo_mikroislem_i[`BIRIM] == `BIRIM_YAPAYZEKA) ? yapayzeka_sonuc_w:
+                                     (cyo_mikroislem_i[`BIRIM] == `BIRIM_SISTEM   ) ? sistem_sonuc_w   :
                                                                                  32'hxxxx_xxxx;
 
-    assign dallanma_kosulu_w = (mikroislem_i[`DAL] == `DAL_EQ ) ?  lt_ltu_eq_i[0]:
-                               (mikroislem_i[`DAL] == `DAL_NE ) ? !lt_ltu_eq_i[0]:
-                               (mikroislem_i[`DAL] == `DAL_LT ) ?  lt_ltu_eq_i[2]:
-                               (mikroislem_i[`DAL] == `DAL_GE ) ? !lt_ltu_eq_i[2]:
-                               (mikroislem_i[`DAL] == `DAL_LTU) ?  lt_ltu_eq_i[1]:
-                               (mikroislem_i[`DAL] == `DAL_GEU) ? !lt_ltu_eq_i[1]:
-                               (mikroislem_i[`DAL] == `DAL_YOK) ? 1'b0           :
-                                                                  1'b0; // x veya ? yerine 0 cunku dallanma_kosulu surekli okunuyor. Kazayla 1 verirsek gecmis olsun.
+    wire dallanma_kosulu_w = (cyo_mikroislem_i[`DAL] == `DAL_EQ ) ?  cyo_lt_ltu_eq_i[0]:
+                             (cyo_mikroislem_i[`DAL] == `DAL_NE ) ? !cyo_lt_ltu_eq_i[0]:
+                             (cyo_mikroislem_i[`DAL] == `DAL_LT ) ?  cyo_lt_ltu_eq_i[2]:
+                             (cyo_mikroislem_i[`DAL] == `DAL_GE ) ? !cyo_lt_ltu_eq_i[2]:
+                             (cyo_mikroislem_i[`DAL] == `DAL_LTU) ?  cyo_lt_ltu_eq_i[1]:
+                             (cyo_mikroislem_i[`DAL] == `DAL_GEU) ? !cyo_lt_ltu_eq_i[1]:
+                             (cyo_mikroislem_i[`DAL] == `DAL_YOK) ? 1'b0           :
+                                                                    1'b0; // x veya ? yerine 0 cunku dallanma_kosulu surekli okunuyor. Kazayla 1 verirsek gecmis olsun.
 
-    assign program_sayaci_gecerli_o = (buyruk_tipi_i == `JTIP) || ((buyruk_tipi_i == `BTIP) && dallanma_kosulu_w);
+    assign gtr_atlanan_ps_gecerli_o = (cyo_buyruk_tipi_i == `JTIP) || ((cyo_buyruk_tipi_i == `BTIP) && dallanma_kosulu_w);
 
-    assign tahmin_dogru_o = (program_sayaci_gecerli_o == coz_program_sayaci_artmis_i);
+    assign gtr_atlanan_ps_o = amb_sonuc_w;
 
-    assign yeni_program_sayaci_o = amb_sonuc_w;
-    assign buyruk_tipi_o = buyruk_tipi_i;
-    assign program_sayaci_o = program_sayaci_i;
-
-    assign yonlendir_deger_o = rd_deger_sonraki_w;
+    assign cyo_yonlendir_deger_o = rd_deger_sonraki_w;
 
     always @(posedge clk_i) begin
         if(rst_i) begin
-            mikroislem_o <= 0;
-            rd_deger_o   <= 0;
-            rd_adres_o   <= 0;
+            gy_mikroislem_o <= 0;
+            gy_rd_deger_o   <= 0;
+            gy_rd_adres_o   <= 0;
         end
         else begin
-            mikroislem_o <= mikroislem_i;
-            rd_deger_o   <= rd_deger_sonraki_w;
-            rd_adres_o   <= rd_adres_i;
+            gy_mikroislem_o <= cyo_mikroislem_i;
+            gy_rd_deger_o   <= rd_deger_sonraki_w;
+            gy_rd_adres_o  <= cyo_rd_adres_i;
+            gy_ps_artmis_o <= cyo_ps_artmis_i;
         end
     end
 
