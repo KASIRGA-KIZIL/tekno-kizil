41c771d1ef6709b0dad612d57558cf60f6e46575
emreoluk <emreoluk@gmail.com>
2023-02-01 16:06:26 +0100

    amb testi eklendi

---
+++ verilog-simulasyonu/test_amb.py
@@ -0,0 +1,180 @@
+#test_amb.py
+
+import cocotb
+from cocotb.triggers import Timer
+import random
+
+
+@cocotb.test()
+async def test_toplama(dut):
+    # Sabit giris atamalari
+    kontrol = 0
+    dut.kontrol.value = kontrol
+    
+    # Dogruluk kontrolu
+    test_sayisi = 1000
+    for test_idx in range(test_sayisi):
+        deger1  = int(random.random()*2**31)
+        deger2  = int(random.random()*2**31)
+        dut.deger1_i.value = deger1
+        dut.deger2_i.value = deger2
+        sonuc = deger1 + deger2
+        await Timer(2,units="ns")
+        assert(sonuc == dut.sonuc_o.value),f"Beklenen sonuc = {sonuc}\nModul ciktisi = {dut.sonuc_o.value}"
+
+@cocotb.test()
+async def test_cikarma(dut):
+    # Sabit giris atamalari
+    kontrol = 1
+    dut.kontrol.value = kontrol
+    
+    # Dogruluk kontrolu
+    test_sayisi = 1000
+    for test_idx in range(test_sayisi):
+        deger1  = int(random.random()*2**16+2**16)
+        deger2  = int(random.random()*2**16)
+        dut.deger1_i.value = deger1
+        dut.deger2_i.value = deger2
+        sonuc = deger1 - deger2
+        await Timer(2,units="ns")
+        assert(sonuc == dut.sonuc_o.value),f"deger1:{deger1}\ndeger2:{deger2}\nBeklenen sonuc = {sonuc}\nModul ciktisi = {dut.sonuc_o.value}"
+
+@cocotb.test()
+async def test_xor(dut):
+    # Sabit giris atamalari
+    kontrol = 2
+    dut.kontrol.value = kontrol
+    
+    # Dogruluk kontrolu
+    test_sayisi = 1000
+    for test_idx in range(test_sayisi):
+        deger1  = int(random.random()*2**32)
+        deger2  = int(random.random()*2**32)
+        dut.deger1_i.value = deger1
+        dut.deger2_i.value = deger2
+        sonuc = deger1 ^ deger2
+        await Timer(2,units="ns")
+        assert(sonuc == dut.sonuc_o.value),f"Beklenen sonuc = {sonuc}\nModul ciktisi = {dut.sonuc_o.value}"
+
+@cocotb.test()
+async def test_or(dut):
+    # Sabit giris atamalari
+    kontrol = 3
+    dut.kontrol.value = kontrol
+    
+    # Dogruluk kontrolu
+    test_sayisi = 1000
+    for test_idx in range(test_sayisi):
+        deger1  = int(random.random()*2**32)
+        deger2  = int(random.random()*2**32)
+        dut.deger1_i.value = deger1
+        dut.deger2_i.value = deger2
+        sonuc = deger1 | deger2
+        await Timer(2,units="ns")
+        assert(sonuc == dut.sonuc_o.value),f"Beklenen sonuc = {sonuc}\nModul ciktisi = {dut.sonuc_o.value}"
+
+@cocotb.test()
+async def test_and(dut):
+    # Sabit giris atamalari
+    kontrol = 4
+    dut.kontrol.value = kontrol
+    
+    # Dogruluk kontrolu
+    test_sayisi = 1000
+    for test_idx in range(test_sayisi):
+        deger1  = int(random.random()*2**32)
+        deger2  = int(random.random()*2**32)
+        dut.deger1_i.value = deger1
+        dut.deger2_i.value = deger2
+        sonuc = deger1 & deger2
+        await Timer(2,units="ns")
+        assert(sonuc == dut.sonuc_o.value),f"Beklenen sonuc = {sonuc}\nModul ciktisi = {dut.sonuc_o.value}"
+
+@cocotb.test()
+async def test_sll(dut):
+    # Sabit giris atamalari
+    kontrol = 5
+    dut.kontrol.value = kontrol
+    
+    # Dogruluk kontrolu
+    test_sayisi = 1000
+    for test_idx in range(test_sayisi):
+        deger1  = int(random.random()*2**32)
+        deger2  = int(random.random()*2**5)
+        dut.deger1_i.value = deger1
+        dut.deger2_i.value = deger2
+        sonuc = (deger1 << deger2)%2**32
+        await Timer(2,units="ns")
+        assert(sonuc == dut.sonuc_o.value),f"deger1:{deger1}\ndeger2:{deger2}\nBeklenen sonuc = {sonuc}\nModul ciktisi = {dut.sonuc_o.value}"
+
+@cocotb.test()
+async def test_srl(dut):
+    # Sabit giris atamalari
+    kontrol = 6
+    dut.kontrol.value = kontrol
+    
+    # Dogruluk kontrolu
+    test_sayisi = 1000
+    for test_idx in range(test_sayisi):
+        deger1  = int(random.random()*2**32)
+        deger2  = int(random.random()*2**5)
+        dut.deger1_i.value = deger1
+        dut.deger2_i.value = deger2
+        sonuc = deger1 >> deger2
+        await Timer(2,units="ns")
+        assert(sonuc == dut.sonuc_o.value),f"deger1:{deger1}\ndeger2:{deger2}\nBeklenen sonuc = {sonuc}\nModul ciktisi = {dut.sonuc_o.value}"
+
+@cocotb.test()
+async def test_sra(dut):
+    # Sabit giris atamalari
+    kontrol = 7
+    dut.kontrol.value = kontrol
+    
+    # Dogruluk kontrolu
+    test_sayisi = 1000
+    for test_idx in range(test_sayisi):
+        deger1  = int(random.random()*2**32)
+        deger2  = int(random.random()*2**5)
+        dut.deger1_i.value = deger1
+        dut.deger2_i.value = deger2
+        sonuc = deger1 >> deger2
+        await Timer(2,units="ns")
+        assert(sonuc == dut.sonuc_o.value),f"deger1:{deger1}\ndeger2:{deger2}\nBeklenen sonuc = {sonuc}\nModul ciktisi = {dut.sonuc_o.value}"
+
+@cocotb.test()
+async def test_slt(dut):
+    # Sabit giris atamalari
+    kontrol = 8
+    dut.kontrol.value = kontrol
+    
+    # Dogruluk kontrolu
+    test_sayisi = 1000
+    for test_idx in range(test_sayisi):
+        deger1  = int(random.random()*2**32)
+        deger2  = int(random.random()*2**32)
+        lt_ltu  = int(random.random()*2**2)
+        dut.deger1_i.value = deger1
+        dut.deger2_i.value = deger2
+        dut.lt_ltu_i.value = lt_ltu
+        sonuc = 0+(lt_ltu%2) if(deger1 < deger2) else 0
+        await Timer(2,units="ns")
+        assert(sonuc == dut.sonuc_o.value),f"deger1:{deger1}\ndeger2:{deger2}\nBeklenen sonuc = {sonuc}\nModul ciktisi = {dut.sonuc_o.value}"
+
+@cocotb.test()
+async def test_sltu(dut):
+    # Sabit giris atamalari
+    kontrol = 9
+    dut.kontrol.value = kontrol
+    
+    # Dogruluk kontrolu
+    test_sayisi = 1000
+    for test_idx in range(test_sayisi):
+        deger1  = int(random.random()*2**31)
+        deger2  = int(random.random()*2**31)
+        lt_ltu  = int(random.random()*2**2)
+        dut.deger1_i.value = deger1
+        dut.deger2_i.value = deger2
+        dut.lt_ltu_i.value = lt_ltu
+        sonuc = 0+((int(lt_ltu/2))%2) if(deger1 < deger2) else 0
+        await Timer(2,units="ns")
+        assert(sonuc == dut.sonuc_o.value),f"deger1:{deger1}\ndeger2:{deger2}\nBeklenen sonuc = {sonuc}\nModul ciktisi = {dut.sonuc_o.value}"
--- verilog-tasarimi/cekirdek/boru_hatti/yurut/x-buyruklari/sifreleme_birimi/popcount.v
+++ verilog-tasarimi/cekirdek/boru_hatti/yurut/x-buyruklari/sifreleme_birimi/popcount.v
@@ -6,9 +6,10 @@ module popcount(
     input [31:0] deger_i,
     output reg [5:0] bir_sayisi
 );
+integer i;
     always@*begin
         bir_sayisi = 0;
-        for(integer i = 0;i<32;i++)begin
+        for(i = 0;i<32;i = i+1)begin
             if(deger_i[i])begin
                 bir_sayisi = bir_sayisi + 1;
             end
