c77ac39f190c9943900ffd27b4df781398612e27
celuk <seyyid4091@gmail.com>
2023-02-03 14:17:30 +0300

    compressed gecerli kontrolunu sil

--- verilog-tasarimi/cekirdek/boru_hatti/getir/getir.v
+++ verilog-tasarimi/cekirdek/boru_hatti/getir/getir.v
@@ -85,14 +85,11 @@ module getir (
         .yrt_buyruk_ctipi_o    (yrt_buyruk_ctipi)
     );
 
-    // compressed buyruk gecerli mi?
-    reg cmp_gecerli_r = 1'b1;
-
     reg bufferdan_okuyor_next;
     reg bufferdan_okuyor;
     always @(*) begin
         ddb_yanlis_tahmin_o = 1'b0;
-        if(buyruk_ctipi & cmp_gecerli_r) begin
+        if(buyruk_ctipi) begin
             ps_artmis = ps + 1; // son bit yok b10  -> b1  oluyor.
         end else begin
             ps_artmis = ps + 2; // son bit yok b100 -> b10 oluyor.
@@ -213,43 +210,6 @@ module getir (
             end
         endcase
     end
-
-    // compressed buyruklarin gecerliligini kontrol et
-    always @(buyruk_16com) begin
-        casez(buyruk_16com)
-            `C_EBREAK   : cmp_gecerli_r = 1'b1;
-            `C_JR       : cmp_gecerli_r = |buyruk_16com[11:7];
-            `C_JALR     : cmp_gecerli_r = |buyruk_16com[11:7];
-            `C_NOP      : cmp_gecerli_r = 1'b1; // c.nop degilse c.addidir
-            `C_ADDI16SP : cmp_gecerli_r = |{buyruk_16com[12],buyruk_16com[6:2]};
-            `C_AND      : cmp_gecerli_r = 1'b1;
-            `C_SUB      : cmp_gecerli_r = 1'b1;
-            `C_OR       : cmp_gecerli_r = 1'b1;
-            `C_XOR      : cmp_gecerli_r = 1'b1;
-            `C_SRAI     , cmp_gecerli_r = |{buyruk_16com[12],buyruk_16com[6:2]};
-            `C_SRLI     : cmp_gecerli_r = |{buyruk_16com[12],buyruk_16com[6:2]};
-            `C_ANDI     : cmp_gecerli_r = 1'b1;
-            `C_MV       : cmp_gecerli_r = |buyruk_16com[11:7];
-            `C_SLLI     : cmp_gecerli_r = ~buyruk_16com[12] & (|buyruk_16com[6:2]);
-            `C_ADD      : cmp_gecerli_r = |buyruk_16com[11:7];
-            `C_ADDI     : cmp_gecerli_r = 1'b1; // c.addi degilse c.noptur
-            `C_ADDI4SPN : cmp_gecerli_r = |buyruk_16com[12:5];
-            `C_BEQZ     ,
-            `C_BNEZ     : cmp_gecerli_r = 1'b1;
-            `C_J        ,
-            `C_JAL      : cmp_gecerli_r = 1'b1;
-            `C_LI       : |buyruk_16com[11:7];
-            `C_LUI      : cmp_gecerli_r = |{buyruk_16com[12], buyruk_16com[6:2]};
-            `C_LW       : cmp_gecerli_r = 1'b1;
-            `C_LWSP     : cmp_gecerli_r = |buyruk_16com[11:7];
-            `C_SW       : cmp_gecerli_r = 1'b1;
-            `C_SWSP     : cmp_gecerli_r = 1'b1;
-            default     : begin
-                cmp_gecerli_r = 1'b0;
-            end
-        endcase
-    end
-
 
     assign l1b_adr_o = ps_next;
 
